90c2888a3b34e54e1ce744fddfd4111db3445e1d
ApoChagi <56980206+ApoChagi@users.noreply.github.com>
2022-06-04 18:38:39 +0300

    Delete 11dummy_sci_fields.sql

--- dummy data/11dummy_sci_fields.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO scientific_field (`Name`) VALUES ("Physical Chemical and Earth Sciences");
-INSERT INTO scientific_field (`Name`) VALUES ("Humanities and Creative Arts");
-INSERT INTO scientific_field (`Name`) VALUES ("Engineering and Environmental Sciences");
-INSERT INTO scientific_field (`Name`) VALUES ("Education and Human Society");
-INSERT INTO scientific_field (`Name`) VALUES ("Economics and Commerce");
-INSERT INTO scientific_field (`Name`) VALUES ("Mathematical Information and Computing Sciences");									
-INSERT INTO scientific_field (`Name`) VALUES ("Biological and Biotechnological Sciences");
-INSERT INTO scientific_field (`Name`) VALUES ("Medical and Health Sciences");
