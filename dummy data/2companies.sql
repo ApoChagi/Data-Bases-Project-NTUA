ad50e96e10c0e84483190ae4d992bbcd65d93100
ApoChagi <56980206+ApoChagi@users.noreply.github.com>
2022-06-04 14:36:33 +0300

    Add files via upload

---
+++ dummy data/2companies.sql
@@ -0,0 +1,13 @@
+ALTER TABLE organization AUTO_INCREMENT = 1;
+INSERT IGNORE INTO `organization` (`Abbreviation`,`Name`,`Street_Name`,`Street_Number`,`Postcode`,`City`,`Category`,`Budget_Ministry_of_Education`,`Budget_Private_Actions`,`Budget_Equity`)
+VALUES
+  ("MKN","At Sem Corporation","Abel B. Navarro",145,"62341","Piotrków Trybunalski","Company", NULL, NULL, 8709287),
+  ("LYX","Molestie In LLP","Marny M. Emerson",190,"33246","Chongqing","Company",NULL, NULL, 7807989),
+  ("AMR","Cum PC","Anne X. Calhoun",85,"69476","Belfort","Company",NULL,NULL,8027848),
+  ("JJD","Consectetuer Adipiscing Corporation","Galena Z. Rodriquez",191,"78529","Empangeni","Company",NULL,NULL,232805),
+  ("TEF","Purus Foundation","Maxwell D. Steele",19,"54726","Málaga","Company",NULL,NULL ,2115032),
+  ("FCC","Urna Vivamus Ltd","Althea R. Barnes",198,"40825","Nonsan","Company",NULL,NULL,6957837),
+  ("LDS","Massa Lobortis Institute","Galena O. Glenn",81,"69694","Chimbarongo","Company",NULL,NULL,7014183),
+  ("WTH","In Molestie Incorporated","Amanda Q. Ramsey",104,"42493","Kohistan","Company",NULL,NULL,3040853),
+  ("KEO","Nunc PC","Nyssa F. Price",56,"89117","Haveli","Company",NULL,NULL,5484793),
+  ("TDN","Curabitur Dictum Phasellus LLP","Mariam E. Mayo",82,"44781","Salamanca","Company",NULL,NULL,837978);
---
+++ dummy data/3universities.sql
@@ -0,0 +1,13 @@
+ALTER TABLE organization AUTO_INCREMENT = 11;
+INSERT INTO `organization` (`Abbreviation`,`Name`,`Street_Name`,`Street_Number`,`Postcode`,`City`,`Category`,`Budget_Ministry_of_Education`,`Budget_Private_Actions`,`Budget_Equity`)
+VALUES
+  ("UBT","Nulla Eget Metus Ltd","Zeus T. Walter",131,"12872","Port Coquitlam","University",2269494,NULL,NULL),
+  ("NMV","Hendrerit Institute","April C. Owen",119,"56963","Ceuta","University",6271596,NULL,NULL),
+  ("YNQ","Montes Nascetur Ridiculus LLC","Brody W. Franco",27,"45774","Vezirköprü","University",3243483,NULL,NULL),
+  ("QTD","Et Industries","Iliana J. Dickson",192,"90116","Irkutsk","University",4082765,NULL,NULL),
+  ("LVI","Augue LLC","Forrest E. Kaufman",163,"87785","Northampton","University",3540854,NULL,NULL),
+  ("QQV","Justo Nec Incorporated","Aaron F. Chaney",118,"36182","Irpin","University",8645384,NULL,NULL),
+  ("FDU","Ut Tincidunt Foundation","Montana R. Hernandez",49,"73735","Soweto","University",7791525,NULL,NULL),
+  ("EBM","Vel Quam Dignissim Incorporated","Erasmus R. Brady",175,"17319","Mont-de-Marsan","University",5122450,NULL,NULL),
+  ("ECO","Nisi Aenean Foundation","Xander S. Snyder",163,"98441","Igboho","University",6747725,NULL,NULL),
+  ("EDP","Vel Est Limited","Simone J. Bush",131,"75658","Linköping","University",4005495,NULL,NULL);
